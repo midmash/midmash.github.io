a0be1828d58eaa9343a0ac1ed66f2e53ea4ee3ba
Midmash <thehtmelle73@gmail.com>
2014-04-01 15:32:06 -0500

    Update foursquare.js

--- javascript/foursquare.js
+++ javascript/foursquare.js
@@ -7,7 +7,7 @@ var config = {
 };
   
 /* Attempt to retrieve access token from URL. */
-  function doAuthRedirect() {
+/*  function doAuthRedirect() {
     var redirect = window.location.href.replace(window.location.hash, '');
     var url = config.authUrl + 'oauth2/authenticate?response_type=token&client_id=' + config.apiKey +
         '&redirect_uri=' + encodeURIComponent(redirect) +
@@ -22,7 +22,21 @@ var config = {
   } else if ($.bbq.getState('error')) {
   } else {
     doAuthRedirect();
-  }
+  }*/
+  
+function getVenue() {
+	var request = new XMLHttpRequest();
+	var URL = "https://api.foursquare.com/v2/venues/search?near=Decorah,IA&client_id=1PYYLFDJI5YVYXGYCNQE4UJPB1N1B1CSZHK4LGYO0BJPEWQ2&client_secret=WLK1VTXZKQ41GGOI1BZUECIA0UT00QSADMBBKKCIS5VCE5IW&v=20140401";
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status==200);
+		{
+			var A = JSON.parse(request.responseText);
+			console.log(A);
+		}
+	}
+	request.open("GET",URL,true);
+	request.send();
+}  
   
 function lookForFun() { 
 	geocodertwo = new google.maps.Geocoder();
