6533a9f7fb1fcb6c11c63fb2d04b2beee507ea9d
Midmash <thehtmelle73@gmail.com>
2014-04-01 11:30:52 -0500

    Update foursquare.js

--- javascript/foursquare.js
+++ javascript/foursquare.js
@@ -1,3 +1,4 @@
+var geocodertwo; 
  
 var config = {
  apiKey: '1PYYLFDJI5YVYXGYCNQE4UJPB1N1B1CSZHK4LGYO0BJPEWQ2',
@@ -6,15 +7,17 @@ var config = {
 };
   
 function lookForFun() { 
-	var lat = document.getElementById("input-lat").value;
-	var lng = document.getElementById("input-long").value;
+	geocodertwo = new google.maps.Geocoder();
+	var latlngtwo = new google.maps.LatLng(document.getElementById('address').value);
+	var lat = latlngtwo.lat();
+	val lng = latlngtwo.lng();
   /* Query foursquare API for venue recommendations near the current location. */
     $.getJSON(config.apiUrl + 'v2/venues/explore?ll=' + lat + ',' + lng + '&oauth_token=' + window.token, {}, function(data) {
       venues = data['response']['groups'][0]['items'];
       /* Place marker for each venue. */
       for (var i = 0; i < venues.length; i++) {
         /* Get marker's location */
-        var latLng = new google.maps.LatLng(venues[i]['venue']['location']['lat'], venues[i]['venue']['location']['lng']);
+        var latLngthree = new google.maps.LatLng(venues[i]['venue']['location']['lat'], venues[i]['venue']['location']['lng']);
         /* Build icon for each icon */
         var leafletIcon = L.Icon.extend({
           iconUrl: venues[i]['venue']['categories'][0]['icon'],
@@ -25,7 +28,7 @@ function lookForFun() {
         });
         var icon = new leafletIcon();
         var marker = new google.maps.Marker({
-        	position: latLng,
+        	position: latLngthree,
         	title:"Marker"
         }
           /*.bindPopup(venues[i]['venue']['name'], { closeButton: false })
