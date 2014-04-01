7e4f0f16bbdf7f85182de2c9a14c4620fc7d9e0a
Midmash <thehtmelle73@gmail.com>
2014-04-01 18:55:16 -0500

    Update venue.js

--- javascript/venue.js
+++ javascript/venue.js
@@ -13,7 +13,7 @@ function getVenue() {
 		if (request.readyState == 4 && request.status==200);
 		{
 			var A = JSON.parse(request.responseText);
-			console.log(A);
+			console.log(A["response"]["venues"][1]["name"]);
 		}
 	}
 	request.open("GET",URL,true);
