ceaba1ce0d0c3794ad04240645c2ee6601d5316c
Midmash <thehtmelle73@gmail.com>
2014-04-02 00:43:36 -0500

    Update venue.js

--- javascript/venue.js
+++ javascript/venue.js
@@ -41,8 +41,8 @@ function getVenue() {
 					var hereNow = A["response"]["venues"][i]["hereNow"]["count"];
   					console.log(hereNow);
   					
-  					//var allInfo = "Name of Venue: " + venueName + "Number of Check-Ins:  " + checkIns + "Number of People Here Now: " + hereNow;
-  					document.getElementById("venue-recs").innerHTML = "Name of Venue: " + venueName + \n + "Number of Check-Ins:  " + checkIns + \n + "Number of People Here Now: " + hereNow;
+  					var allInfo = "Name of Venue: " + venueName + \n + "Number of Check-Ins:  " + checkIns + \n + "Number of People Here Now: " + hereNow;
+  					document.getElementById("venue-recs").innerHTML = allInfo;
   					//document.getElementById("venue-recs").innerHTML = "Number of Check-Ins:  " + checkIns;
   					//document.getElementById("venue-recs").innerHTML = "Number of People Here Now: " + hereNow;
   				});
