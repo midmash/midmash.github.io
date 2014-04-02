1ea1779fc47b5065ef1ff7f70ca3adb43581265b
Midmash <thehtmelle73@gmail.com>
2014-04-02 02:20:26 -0500

    Update venue.js

--- javascript/venue.js
+++ javascript/venue.js
@@ -33,7 +33,7 @@ function getVenue() {
     				map.setZoom(8);
 				map.setCenter(marker.getPosition());
 					
-				var venueName = A["response"]["venues"][i]["name"];
+				var venueName = marker.(A["response"]["venues"][i]["name"]);
 				console.log(venueName);
 					
 				var checkIns = A["response"]["venues"][i]["stats"]["checkinsCount"];
