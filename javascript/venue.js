17f0c83c906cf8e10a099a9fc75fd7104363e997
Midmash <thehtmelle73@gmail.com>
2014-04-02 18:38:17 -0500

    Update venue.js

--- javascript/venue.js
+++ javascript/venue.js
@@ -24,13 +24,13 @@ function getVenue() {
         				title: "Venue Name: " + venueName + "\n" + "Number of Check-In's: " + checkIns + "\n" + "Number of Users to Check-In Here: " + userChecks
 					 })
 					 
+				marker.setMap(map);
+				map.setCenter(marker.getPosition());
+				
 				var infowindow = new google.maps.InfoWindow({
 					content: "Venue Name: " + venueName + "<br>" + "Number of Check-In's: " + checkIns + "<br>" + "Number of Users to Check-In Here: " + userChecks,
 					map: map,
-					})
-					 
-				marker.setMap(map);
-				map.setCenter(marker.getPosition());
+				})
 				
 				infowindow.open(map, marker);
 			}
