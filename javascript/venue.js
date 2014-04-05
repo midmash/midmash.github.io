1f7a412510b3ade86ee23d841052514c8019684e
Midmash <thehtmelle73@gmail.com>
2014-04-05 01:30:38 -0500

    Update venue.js

--- javascript/venue.js
+++ javascript/venue.js
@@ -7,7 +7,7 @@ function getVenue() {
 	var request = new XMLHttpRequest();
 	var URL = "https://api.foursquare.com/v2/venues/search?near=" + document.getElementById('address').value + "&client_id=1PYYLFDJI5YVYXGYCNQE4UJPB1N1B1CSZHK4LGYO0BJPEWQ2&client_secret=WLK1VTXZKQ41GGOI1BZUECIA0UT00QSADMBBKKCIS5VCE5IW&v=20140401";
 	request.onreadystatechange = function() {
-		if (request.readyState == 4 && request.status==200);
+		if (request.readyState == 4 && request.status==200)
 		{
 			var A = JSON.parse(request.responseText);
 			console.log(A);
